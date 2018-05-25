d58395def599c372379b77110c1a8c8dfb633ea2
Cody Swain <codyswain@codys-mbp.lan>
2018-05-24 17:43:41 -0700

    seperated HTML and heroku app

--- app.py
+++ app.py
@@ -4,14 +4,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
+	return render_template('homepage.html')
+    # the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
 
-    return """
-    <h1>Hello heroku</h1>
-    <p>It is currently {time}.</p>
+    # return """
+    # <h1>Hello heroku</h1>
+    # <p>It is currently {time}.</p>
 
-    <img src="http://loremflickr.com/600/400">
-    """.format(time=the_time)
+    # <img src="http://loremflickr.com/600/400">
+    # """.format(time=the_time)
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
