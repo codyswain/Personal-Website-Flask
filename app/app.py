ada8076b3928b157b84c9dc630d2cd8b49ee9be1
Cody Swain <codyswain@codys-mbp.lan>
2018-05-25 02:26:30 -0700

    attempt3

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-	return 'homepage.html'
+	return 'app/homepage.html'
     # the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
 
     # return """
