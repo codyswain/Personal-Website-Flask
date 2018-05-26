22e9946bce0e3c1e30487297c0e7ad0382655e65
Cody Swain <swain.cody@gmail.com>
2018-05-26 03:07:15 -0700

    Bootstrap

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-	return render_template("homepage.html")
+	return render_template("index.html")
     # the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
 
     # return """
